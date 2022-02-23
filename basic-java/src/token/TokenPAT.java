63bfadc2fcd72b66bb890464563a1d2cf4c97132
tuhinal <alauddintuhin1411@gmail.com>
2022-02-23 22:08:34 +0600

    added to

---
+++ basic-java/src/token/TokenPAT.java
@@ -0,0 +1,7 @@
+package token;
+
+public class TokenPAT {
+    public static void main(String[] args) {
+        System.out.println("-leetcodeToken23-02-2022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-dateExpire23-05-2022");
+    }
+}
