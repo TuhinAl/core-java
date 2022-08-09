7e406d2bd03ad26991a6288d6b8cbb4592ae378b
tuhinal <alauddintuhin1411@gmail.com>
2022-08-10 00:18:50 +0600

    exception test

---
+++ basic-java/src/exception/ExceptionDemo.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ExceptionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Exception Demo");
+
+	}
+
+}
