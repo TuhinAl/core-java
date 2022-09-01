7661bc2cd06296b5ac2f7739d72c1333280101f1
tuhinal <alauddintuhin1411@gmail.com>
2022-09-01 12:53:02 +0600

    leetcode solution by me

--- basic-java/src/leetcode/Solution.java
+++ basic-java/src/leetcode/Solution.java
@@ -1,43 +1,46 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Solution {
 	
-	public static String[] reverseString(String[] str){
-        int left = 0;
-        int right = str.length-1;
-        while(left < right){
-//        	System.out.println("print");
-            String temp = str[left];
-            str[left] = str[right];
-            str[right] = temp;
-            left++;
-            right--;
-        }
-//        System.out.println("print");
-        System.out.println(str);
-            return str;
-    }
-    
-    
-    public static void main(String []args){
-        
-//    Solution solution = new Solution();
-    Scanner scanner = new Scanner(System.in);
-    int arrayLength = scanner.nextInt();
-    String str [] = new String[arrayLength];
-    for(int i = 0; i < arrayLength; i++){
-        
-         str[i] = scanner.nextLine();
-    }
-//    scanner.close();
-//    
-//    String str [] = {"h","e","l","l","o"};
-   String s[] = Solution.reverseString(str);
-   
-   for(int i = 0; i < s.length; i++) {
-	   System.out.print(s[i]);
-   }
-    }
+	 public static List<String> findRepeatedDnaSequences(String s) {
+	        int len = s.length();
+	        Map<String, Integer> map = new HashMap<>();
+	        List<String> result = new ArrayList<>();
+	        int i = 0;
+	        while(i < len-9) {
+	        	String subString = s.substring(i,i+10);
+	        	if(!map.containsKey(subString)) {
+	        		map.put(subString, 1);
+	        	}else {
+	        		map.put(subString, map.get(subString)+1);
+	        	}
+	        	i++;
+	        }
+	        
+	        Set<Map.Entry<String, Integer>> mapToList = map.entrySet();
+	        mapToList.stream().filter(value -> value.getValue() >= 1)
+	        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	        
+	        mapToList.stream().filter(p -> p.getValue()>1).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	        for(Map.Entry<String, Integer> maps : mapToList) {
+	        	if(maps.getValue() > 1) {
+	        		result.add(maps.getKey());
+	        	}
+	        }
+	        System.out.println(result);
+	     return result;   
+	    }
+	 
+	 public static void main(String[] args) {
+		Solution.findRepeatedDnaSequences("AAAAAAAAAAA");
+	}
 }
